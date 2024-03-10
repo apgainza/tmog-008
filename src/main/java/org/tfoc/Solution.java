f74e8e2ea864c2e72cdc0b8a56938a51778c3222
Alejandro Portilla Gainza <aportiga@mercadona.es>
2024-03-10 16:23:48 +0100

    feat: Solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,37 @@
 package org.tfoc;
 
+import java.util.Objects;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
+    int left, right;
+
     public Integer search(
             Integer[] nums,
             Integer target) {
 
-        return null;
+        left = 0;
+        right = nums.length - 1;
+
+        return binarySearch(nums, target);
     }
 
+    private Integer binarySearch(Integer[] nums, Integer target) {
+        int m = (right + left) / 2;
+        if (left > right)
+            return null;
+
+        if (Objects.equals(nums[m], target))
+            return m;
+
+        if (nums[m] > target)
+            right = m - 1;
+        else
+            left = m + 1;
+
+        return binarySearch(nums, target);
+    }
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,34 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    private static Stream<Arguments> provideBinarySearch() {
+        return Stream.of(
+                Arguments.of(new Integer[]{-1, 0, 3, 5, 9, 12}, 9, 4),
+                Arguments.of(new Integer[]{2, 5, 8, 12, 16, 23, 38, 56, 72, 91}, 23, 5),
+                Arguments.of(new Integer[]{2, 3, 4, 10, 40}, 10, 3)
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideBinarySearch")
+    void testSolution(Integer[] nums, Integer target, Integer posExpected) {
+        Solution solution = new Solution();
+
+        Integer search = solution.search(nums, target);
+
+        Assertions.assertEquals(search, posExpected);
+
     }
 
 }
